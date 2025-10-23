adc4087b428c63d8aa95c3ec9ca7cf7a2d5a78ed
Алексей Липницкий <Lipnickij_AP_25@student.grsu.by>
2025-10-23 10:42:12 +0300

    Add files via upload

---
+++ lab5/lab5_22.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	char letter;
+
+	cout << "Введите одну из 6 букв ( P U R L M ):";
+	cin >> letter;
+
+	switch (letter) {
+	case 'P':
+		cout << "Portugal" << endl;
+		break;
+	case 'U':
+		cout << "Ukraine" << endl;
+		break;
+	case 'R':
+		cout << "Russia" << endl;
+		break;
+	case 'L':
+		cout << "Latvia" << endl;
+		break;
+	case 'M':
+		cout << "Montenegro" << endl;
+		break;
+	default:
+		cout << "Ошибка!" << endl;
+		break;
+	}
+	return 0;
+}
